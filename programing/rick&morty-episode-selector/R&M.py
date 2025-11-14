b5d1da9b3a9983e7588e1339cc67bb73329dd990
RGO <rgokristof10@gmail.com>
2025-11-14 22:57:36 +0100

    as

--- programing/rick&morty-episode-selector/R&M.py
+++ programing/rick&morty-episode-selector/R&M.py
@@ -1,22 +1,30 @@
-import webbrowser
+import webbrowser as web
 import time
 import pyautogui
 import keyboard
+
+def wait(timetowait):
+    time.sleep(timetowait)
 
 linkStart = "https://play.hbomax.com/video/watch/"
 
 file = open("./programing/rick&morty-episode-selector/R&M-data.txt")
 lines = file.readlines()
 
-season = str(input("Season: "))
-episode = str(input("Episode: "))
-
+stop = 0
 for i in range(len(lines)):
-    if lines[i].split(".")[0] == season+episode:
-        print(f"Epizode name:{lines[i].split(".")[1]}Epizode length:{lines[i].split(".")[2]}")
-        print("Episode starts in...")
-        for i2 in range(3):
-            print(str(i2+1))
-            time.sleep(1)
-        webbrowser.open(linkStart+lines[i].split(".")[3].strip())
-time.sleep(10)
+    if stop == 0:
+        link = lines[i].split(".")[3].strip()
+        web.open(linkStart+link)
+        wait(2)
+        w = 1
+        while w == 1:
+            wait(1)
+            if keyboard.is_pressed("control+alt+s"):
+                stop = 1
+                w = 0
+            elif keyboard.is_pressed("control+alt+n"):
+                w = 0
+    else:
+        print("stopped")
+        break
